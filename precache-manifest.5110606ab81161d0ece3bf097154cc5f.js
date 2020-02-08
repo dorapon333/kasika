9235122b398a59ceb1a0d7c59d989d0eb2c2cd99
dorapon333 <esutaku18781@gmail.com>
2020-02-09 08:35:13 +0900

    Updates

---
+++ precache-manifest.5110606ab81161d0ece3bf097154cc5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3692e3c5d7b02ad2d60f661eca0a139",
+    "url": "/kasika/index.html"
+  },
+  {
+    "revision": "e516df9906e8ed6231a1",
+    "url": "/kasika/static/js/2.58eb5596.chunk.js"
+  },
+  {
+    "revision": "a0a3063ed0e7e344881a79d4bea9f313",
+    "url": "/kasika/static/js/2.58eb5596.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "207630a77eb9f905f3ec",
+    "url": "/kasika/static/js/main.750d4e92.chunk.js"
+  },
+  {
+    "revision": "ad42386242fe54398395",
+    "url": "/kasika/static/js/runtime-main.141f653a.js"
+  }
+]);
